58f8f8686e335bbe6da64b9c8d923c68acb1134a
M R Saad <mshad221487@bscse.uiu.ac.bd>
2023-02-25 22:32:47 +0600

    Method

--- start/src/main/java/login/m_controller.java
+++ start/src/main/java/login/m_controller.java
@@ -1,8 +1,15 @@
 package login;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class m_controller {
+
+    @FXML
+
+    public void logOut(ActionEvent action){
+
+    }
 
 }
