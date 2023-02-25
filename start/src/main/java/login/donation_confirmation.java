b0c201082294447f86b83e91553b870ac55522fc
draudrichman <88120992+draudrichman@users.noreply.github.com>
2023-02-25 21:21:33 +0600

    first few frames

---
+++ start/src/main/java/login/donation_confirmation.java
@@ -0,0 +1,24 @@
+package login;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class donation_confirmation extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(login.class.getResource("donation_confirmation.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        stage.setTitle("Donation Confirmation");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
