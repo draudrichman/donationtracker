f35c2e173549478ad90a14102e196934cf4a0190
viktor <124088744+M-R-Saad@users.noreply.github.com>
2023-04-15 00:16:06 +0600

    Update 3

--- src/main/java/login/Client.java
+++ src/main/java/login/Client.java
@@ -19,10 +19,4 @@ public class Client {
             System.out.println(e);
         }
     }
-
-    public static void main(String args[]) {
-        String serverAddress = "127.0.0.1";
-        int serverPort = 33333;
-        Client client = new Client(serverAddress, serverPort);
-    }
 }
