7d2a934bc19ce3139b162e18c5cb8a612235ec19
draudrichman <88120992+draudrichman@users.noreply.github.com>
2023-04-28 20:46:09 +0600

    homepage

--- src/main/java/login/Constants.java
+++ src/main/java/login/Constants.java
@@ -1,9 +1,9 @@
 package login;
 
 public class Constants {
-    public static final String DATABASE_URL = "jdbc:mysql://localhost:3306/donation_tracker";
-    public static final String DATABASE_USERNAME = "root";
-    public static final String DATABASE_PASSWORD = "";
+    public static final String DATABASE_URL = "jdbc:mysql://localhost:3308/donation_tracker";
+    public static final String DATABASE_USERNAME = "chisea";
+    public static final String DATABASE_PASSWORD = "1234";
 
     public static final String SERVER_ADDRESS = "127.0.0.1";
 
